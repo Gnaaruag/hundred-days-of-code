a708b7ed87f6bdd52a3e947062b329bec9ca392c
Gnaaruag <68043860+Gnaaruag@users.noreply.github.com>
2022-09-27 20:36:44 +0530

    Create removeelement.java

---
+++ submissions-for-online-judges/removeelement.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/submissions/detail/809053446/
+// 26.09
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k ;
+    }
+}
