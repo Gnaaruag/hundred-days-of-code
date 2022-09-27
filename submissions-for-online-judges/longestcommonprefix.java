2f10f615281b9d2b9ec1d72e34df17816db34ae1
Gnaaruag <68043860+Gnaaruag@users.noreply.github.com>
2022-09-27 20:28:23 +0530

    Create longestcommonprefix.java

---
+++ submissions-for-online-judges/longestcommonprefix.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/submissions/detail/807545218/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        int n = strs.length;
+        String s = strs[0];
+        String l = strs[n-1];
+        int i = 0;
+        while (i < s.length()) {
+            if (s.charAt(i) == l.charAt(i)) {
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        return (i == 0) ? "" : s.substring(0,i);
+}
+}
